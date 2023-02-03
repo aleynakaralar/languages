faafb91b4892fc83901de294d7acdb3057077ec0
Aleyna <85024652+aleynakaralar@users.noreply.github.com>
2023-02-03 17:27:22 +0300

    update css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Card from './components/card/Card';
 import Header from './components/header/Header';
@@ -9,6 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <div className='container'>
+        <h1>Languages</h1>
         {data.map((item, index) => {
           const {name,img,options} = item
           return <div className='card-info'> <Card key={index} name={name} img={img} options={options}/> </div>
